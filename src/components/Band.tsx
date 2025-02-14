36240d350a7b6486e1711fa4b29c28219706279a
Volodymyr <volodyaruzhak@gmail.com>
2025-02-14 18:29:01 -0500

    - Worked on band info text and short about

--- src/components/Band.tsx
+++ src/components/Band.tsx
@@ -6,7 +6,7 @@ const Band = () => {
         <div className="flex flex-col items-center w-6/12 m-auto py-10">
             {/* <h1 className="text-5xl font-bold">ARTIFACT</h1><br/> */}
             <img src={Artifact} alt="Artifact" className="w-5/12 h-[170px] mb-4" />
-            <p>{bandInfo.overviewOne}</p>< br/>
+            <p>{bandInfo.earlyYearsAndLineup}</p>< br/>
             <div className="flex py-4">
                 <p className="pr-4">{bandInfo.overviewTwo}</p><br/>
                 <img src={DeadSilence} alt="Dead Silence" className="w-4/12"/>
