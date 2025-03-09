c7c429437649ff62cce8d8d23f692efee304f0a8
Volodymyr <volodyaruzhak@gmail.com>
2025-03-09 18:36:39 -0400

    -Fix GoToTop margin issue on small devices

--- src/components/GoToTop.tsx
+++ src/components/GoToTop.tsx
@@ -15,7 +15,7 @@ export const GoToTop = ({ isHidden }: { isHidden: boolean }) => {
     return (
         <button
             onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-            className={`fixed bottom-0 left-6 bg-gray-700 text-white px-5 sm:px-3 py-2 sm:py-1 rounded-t mt-1 mb-2 text-sm transition-opacity duration-300 z-[60] ${
+            className={`fixed bottom-0 left-6 bg-gray-700 text-white px-5 sm:px-3 py-2 sm:py-1 rounded-t mt-1 mb-0 sm:mb-2 text-sm transition-opacity duration-300 z-[60] ${
                 isVisible && !isHidden ? "opacity-100" : "opacity-0 pointer-events-none"
             }`}
             aria-label="Go to Top"
